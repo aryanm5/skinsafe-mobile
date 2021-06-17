29dcdea530182b1038f740045ebd46c6cd621726
aryanm5 <aryanm.email@gmail.com>
2021-06-16 22:02:15 -0700

    added app icon, launch screen, and installed on device

--- ios/SkinSafe/AppDelegate.m
+++ ios/SkinSafe/AppDelegate.m
@@ -32,6 +32,11 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  //UIStoryboard *sb = [UIStoryboard storyboardWithName:@"LaunchScreen" bundle:nil];
+  //UIViewController *vc = [sb instantiateInitialViewController];
+  //rootView.loadingView = vc.view;
+  
   return YES;
 }
 
