27f10c4448262e5f2e82bf77175244729d87b2d3
aryanm5 <aryanm.email@gmail.com>
2021-06-19 14:34:20 -0700

    re-enabled warning

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import {AppRegistry} from 'react-native';
 import AppWrapper from './src/AppWrapper';
 import {name as appName} from './app.json';
 
-console.disableYellowBox = true;
+//console.disableYellowBox = true;
 
 AppRegistry.registerComponent(appName, () => AppWrapper);
